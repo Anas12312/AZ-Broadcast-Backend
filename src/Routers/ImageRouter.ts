6ac1f8641e033dba951f9e4d75a7159ad87f816e
Zyad Abdul-Nasser <zizo_007@live.com>
2024-02-18 22:05:20 +0200

    z

---
+++ src/Routers/ImageRouter.ts
@@ -0,0 +1,37 @@
+import { Request, Response, Router } from "express";
+import * as ftp from 'basic-ftp'
+import { randomUUID } from "crypto";
+import { Readable } from "stream";
+import fileUpload from 'express-fileupload'
+import cloudinary from 'cloudinary'
+import { resolve } from "path";
+
+const imageRouter = Router();
+
+imageRouter.post('/upload/', fileUpload(), async (req, res) => {
+    try {
+
+        const img = req.files!.file as any
+        cloudinary.v2.config({
+            cloud_name: 'dknhrwczx',
+            api_key: '177672793956226',
+            api_secret: 'R3kZjwFoEhTfxF19D4uWTdAEnZM',
+            secure: true,
+        });
+
+        const result = await new Promise((resolve) => {
+            cloudinary.v2.uploader.upload_stream((error, uploadResult) => {
+                return resolve(uploadResult);
+            }).end(img.data);
+        }) 
+
+        res.send((result as any).url);
+    } catch (e) {
+        console.log(e);
+        res.status(500).send(e)
+
+    }
+})
+
+
+export default imageRouter;
