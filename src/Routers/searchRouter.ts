36be4dc1f06e3dbfef141a12cb3b4d81fec03c38
anas12312 <anashesham000@gmail.com>
2024-03-16 23:46:21 +0200

    anas

--- src/Routers/searchRouter.ts
+++ src/Routers/searchRouter.ts
@@ -1,5 +1,6 @@
 import { Request, Response, Router } from "express";
 import yts from "yt-search";
+import search from "youtube-search";
 const searchRouter = Router();
 
 searchRouter.post('/yt', async (req, res) => {
@@ -20,6 +21,27 @@ searchRouter.post('/yt', async (req, res) => {
         }
     })
     res.send(videos)
+})
+searchRouter.post('/yt2', async (req, res) => {
+    const { searchTerm } = req.body
+    search(searchTerm, {
+        maxResults: 20,
+        key: 'AIzaSyChSu46WcWiwumc1QKz3iShpjXNmWxOywE',
+        videoDuration: "any",
+        type: "video"
+    },  (err, results) => {
+        if(err) return res.status(400).send({message: err.message})
+        const videos = results?.map((video) => {
+            return {
+                name: video.title,
+                url: video.link,
+                thumbnail: video.thumbnails.high? video.thumbnails.high.url: video.thumbnails.default,
+                author: video.channelTitle,
+                // duration: video.seconds
+            }
+        })
+        res.send(videos)
+    })
 })
 
 export default searchRouter;
